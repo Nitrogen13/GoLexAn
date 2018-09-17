36619e1c0c987d96e60e5cffe98cf32b3e48e159
Ilgizar <ilgizar1313@gmail.com>
2018-09-18 02:29:27 +0300

    Dictionary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,11 @@
 import java.io.File
 
 val letterRegex = Regex("[a-zA-Z]")
-val identifier = Regex("[\\w\\d_]")
-val numRegex = Regex("[\\d]")
+val identifier = Regex("[a-zA-Z][\\w\\d_]*")
+val numRegex = Regex("\\d")
 val bracketRegex = Regex("[(){}\\[\\]]")
-val whitespaceRegex = Regex("\\s")
-
-// TODO fix regex for hex numbers
-//val hexNumRegex = Regex("0x[a-fA-F0-9]{8}")
+val whiteSpaceRegex = Regex("[\\s]")
+val pointerRegex = Regex("\\*[a-zA-Z][\\w\\d_]*")
 
 val keywords = listOf(
         "break", "default", "func", "interface", "select",
@@ -17,20 +15,87 @@ val keywords = listOf(
         "continue", "for", "import", "return", "var"
 )
 
+val dictionary = hashMapOf(
+        "break" to "Keyword",
+        "default" to "Keyword",
+        "func" to "Keyword",
+        "interface" to "Keyword",
+        "select" to "Keyword",
+        "case" to "Keyword",
+        "defer" to "Keyword",
+        "go" to "Keyword",
+        "map" to "Keyword",
+        "struct" to "Keyword",
+        "chan" to "Keyword",
+        "else" to "Keyword",
+        "goto" to "Keyword",
+        "package" to "Keyword",
+        "switch" to "Keyword",
+        "const" to "Keyword",
+        "fallthrough" to "Keyword",
+        "if" to "Keyword",
+        "range" to "Keyword",
+        "type" to "Keyword",
+        "continue" to "Keyword",
+        "for" to "Keyword",
+        "import" to "Keyword",
+        "return" to "Keyword",
+        "var" to "Keyword",
+        "==" to "Comparison operator sign",
+        "!=" to "Comparison operator sign",
+        "<" to "Comparison operator sign",
+        "<=" to "Comparison operator sign",
+        ">" to "Comparison operator sign",
+        ">=" to "Comparison operator sign",
+        "+" to "Sum",
+        "-" to "Difference",
+        "*" to "Product",
+        "/" to "Quotient",
+        "%" to "Remainder",
+        "&" to "Bitwise AND",
+        "|" to "Bitwise OR",
+        "^" to "Bitwise XOR",
+        "&^" to "Bit clear (AND NOT)",
+        "<<" to "Left shift",
+        ">>" to "Right shift",
+        "&&" to "Conditional AND",
+        "||" to "Conditional OR",
+        "!" to "NOT",
+        "," to "Separator",
+        "." to "Separator",
+        ";" to "Separator",
+        ":" to "Separator",
+        ":=" to "Assign operator",
+        "/=" to "Assign operator",
+        "+=" to "Assign operator",
+        "-=" to "Assign operator",
+        "%=" to "Assign operator",
+        "*=" to "Assign operator",
+        ">>=" to "Assign operator", // don't know what is this operator exactly
+        "^=" to "Assign operator",
+        "&=" to "Assign operator",
+        "|=" to "Assign operator",
+        "&^=" to "Assign operator",
+        "..." to "3 dot operator",
+        "<-" to "Arrow",
+        "++" to "Increase",
+        "--" to "Decrease"
+)
+
 fun main(args: Array<String>) {
     val lines = File("in.txt").readText()
 
     var pos = 0
     do {
         val token = getToken(lines, pos)
-        println("\"$token\"")
+        println(dict(token))
         token?.length?.let { pos += it }
     } while (token?.isNotEmpty() == true && pos < lines.length)
 }
 
 fun getToken(line: String, pos: Int): String? =
         when {
-            whitespaceRegex.matches(line[pos].toString()) -> line.sliceWhile(pos, whitespaceRegex) //spaces, new lines, tabs
+            whiteSpaceRegex.matches(line[pos].toString()) -> line.sliceWhile(pos, whiteSpaceRegex) //spaces, new lines, tabs
             letterRegex.matches(line[pos].toString()) -> { //character
                 keywords.firstOrNull {
                     line.slice(pos until (pos + it.length).coerceAtMost(line.length)) == it // keyword
@@ -54,7 +119,7 @@ fun getToken(line: String, pos: Int): String? =
             else -> when (line[pos]) {
                 '"' -> {
                     var num = line.sliceUpTo(pos, '"')
-                    while (line[pos + num!!.length - 1] != '"' || line[pos + num.length  - 2] == '\\'){
+                    while (line[pos + num!!.length - 1] != '"' || line[pos + num.length - 2] == '\\') {
                         num += line[pos + num.length]
                     }
                     num
@@ -109,11 +174,11 @@ fun getToken(line: String, pos: Int): String? =
                         "/" + line.sliceUpTo(pos + 1, '\n')
                     }
                     '*' -> {
-                            var num = "*"
-                            while (line[pos + num.length - 1] != '*' || line[pos + num.length] != '/'){
-                                num += line[pos + num.length + 1]
-                            }
-                            num
+                        var num = "*"
+                        while (line[pos + num.length - 1] != '*' || line[pos + num.length] != '/') {
+                            num += line[pos + num.length + 1]
+                        }
+                        num
                     }
                     '=' -> line[pos + 1].toString()
                     else -> ""
@@ -146,6 +211,18 @@ fun getToken(line: String, pos: Int): String? =
                 else -> null
             }
         }
+
+private fun dict(token: String?) : String?{
+    return when{
+        dictionary[token] != null -> dictionary[token]
+        bracketRegex.matches(token.toString()) -> "Bracket"
+        numRegex.matches(token.toString()) -> "Number"
+        whiteSpaceRegex.matches(token.toString()) -> "Whitespace or new line or tab"
+        pointerRegex.matches(token.toString()) -> "Pointer"
+        identifier.matches(token.toString()) -> "Identifier"
+        else -> null // this is tab in most of cases TODO somehow handle it
+    }
+}
 
 private fun String.sliceUpTo(startIndex: Int, matcher: (Char) -> Boolean): String? {
     var i = startIndex + 1

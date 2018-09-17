760e017993218369baa8d3fa0bb48466f0b35ca2
Timur <minteemer@gmail.com>
2018-09-17 22:05:13 +0300

    Code refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 import java.io.File
 
-val charRegex = Regex("\\w")
+val letterRegex = Regex("\\w")
 val identifier = Regex("[\\w\\d_]")
 val numRegex = Regex("[\\d]")
 val bracketRegex = Regex("[(){}\\[\\]]")
+val whitespaceRegex = Regex("\\s")
 
 // TODO fix regex for hex numbers
 //val hexNumRegex = Regex("0x[a-fA-F0-9]{8}")
@@ -19,123 +20,129 @@ val keywords = listOf(
 fun main(args: Array<String>) {
     val lines = File("in.txt").readText()
 
-    val input = "$lines  " // TODO fix
     var pos = 0
-
     do {
-        val token = getToken(input, pos)
-        println("$token|")
+        val token = getToken(lines, pos)
+        println("\"$token\"")
         token?.length?.let { pos += it }
-    } while (token?.isNotEmpty() == true && pos + 1 <= lines.length)
+    } while (token?.isNotEmpty() == true && pos < lines.length)
 }
-
 
 fun getToken(line: String, pos: Int): String? =
         when {
-            Regex("\\s").matches(line[pos].toString()) -> " " //spaces, new lines, tabs
-            charRegex.matches(line[pos].toString()) -> { //character
+            whitespaceRegex.matches(line[pos].toString()) -> line.sliceWhile(pos, whitespaceRegex) //spaces, new lines, tabs
+            letterRegex.matches(line[pos].toString()) -> { //character
                 keywords.firstOrNull {
-                    line.slice(pos until pos + it.length) == it // keyword
-                } ?: run {
-                    var i = pos + 1
-                    while (identifier.matches(line[i].toString()))
-                        ++i
-                    line.slice(pos until i) // identifier
-                }
+                    line.slice(pos until (pos + it.length).coerceAtMost(line.length)) == it // keyword
+                } ?: line.sliceWhile(pos, identifier)// identifier
             }
             bracketRegex.matches(line[pos].toString()) -> line[pos].toString()
-            numRegex.matches(line[pos].toString()) -> { // number
-                var i = pos + 1
-                while (numRegex.matches(line[i].toString()))
-                    ++i
-                line.slice(pos until i)
-            }
-            line[pos] == '"' -> { // String
-                var i = pos + 1
-                while (line[i] != '"')
-                    ++i
-                line.slice(pos..i)
-            }
-            line[pos].toString() == "'" -> { // String
-                var i = pos + 1
-                while (line[i].toString() != "'")
-                    ++i
-                line.slice(pos..i)
-            }
-            line[pos] == '+' -> "+" + when (line[pos + 1]) {
-                '+', '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '-' -> "-" + when (line[pos + 1]) {
-                '-', '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '*' -> "*" + when  {
-                line[pos + 1] == '=' -> line[pos + 1].toString()
-                identifier.matches(line[pos + 1].toString()) -> {
-                    var i = pos + 1
-                    while (identifier.matches(line[i].toString()))
-                        ++i
-                    line.slice(pos until i) // pointer
+            numRegex.matches(line[pos].toString()) -> line.sliceWhile(pos, numRegex) // number
+            else -> when (line[pos]) {
+                '"' -> line.sliceUpTo(pos, '"') // String
+                '\'' -> line.sliceUpTo(pos, '\'') // Chars
+
+                ',' -> ","
+                ';' -> ";"
+
+                '+' -> "+" + when (line[pos + 1]) {
+                    '+', '=' -> line[pos + 1].toString()
+                    else -> ""
                 }
-                else -> ""
-            }
-            line[pos] == ',' -> ","
-            line[pos] == ';' -> ";"
-            line[pos] == '.' -> "." + when (line[pos + 1]) {
-                '.' -> ".."
-                else -> ""
-            }
-            line[pos] == '!' -> "!" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '%' -> "%" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '^' -> "^" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == ':' -> ":" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '/' -> "/" + when (line[pos + 1]) {
-                '/', '=', '*' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '&' -> "&" + when (line[pos + 1]) {
-                '&', '=' -> line[pos + 1].toString()
-                '^' -> "^="
-                else -> ""
-            }
-            line[pos] == '=' -> "=" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '|' -> "|" + when (line[pos + 1]) {
-                '|', '=' -> line[pos + 1].toString()
-                else -> ""
-            }
-            line[pos] == '<' -> "<" + when (line[pos + 1]) {
-                '-', '=' -> line[pos + 1].toString()
-                '<' -> "<" + when (line[pos + 1]) {
+                '-' -> "-" + when (line[pos + 1]) {
+                    '-', '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                '*' -> "*" + when {
+                    line[pos + 1] == '=' -> line[pos + 1].toString()
+                // TODO: Shouldn't it be letterRegexp? Isn't it another token?
+                    identifier.matches(line[pos + 1].toString()) -> line.sliceWhile(pos + 1, identifier)
+                    else -> ""
+                }
+                '.' -> "." + when (line[pos + 1]) {
+                    '.' -> ".."
+                    else -> ""
+                }
+
+            // equals:
+                '!' -> "!" + when (line[pos + 1]) {
                     '=' -> line[pos + 1].toString()
                     else -> ""
                 }
-                else -> ""
-            }
-            line[pos] == '>' -> ">" + when (line[pos + 1]) {
-                '=' -> line[pos + 1].toString()
+                '%' -> "%" + when (line[pos + 1]) {
+                    '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                '^' -> "^" + when (line[pos + 1]) {
+                    '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                ':' -> ":" + when (line[pos + 1]) {
+                    '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                '=' -> "=" + when (line[pos + 1]) {
+                    '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+
+                '/' -> "/" + when (line[pos + 1]) {
+                // TODO: /* - is part of comment
+                    '/', '=', '*' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                '&' -> "&" + when (line[pos + 1]) {
+                    '&', '=' -> line[pos + 1].toString()
+                    '^' -> "^=" // TODO: ???
+                    else -> ""
+                }
+                '|' -> "|" + when (line[pos + 1]) {
+                    '|', '=' -> line[pos + 1].toString()
+                    else -> ""
+                }
+                '<' -> "<" + when (line[pos + 1]) {
+                    '-', '=' -> line[pos + 1].toString()
+                    '<' -> "<" + when (line[pos + 1]) {
+                        '=' -> line[pos + 1].toString()
+                        else -> ""
+                    }
+                    else -> ""
+                }
                 '>' -> ">" + when (line[pos + 1]) {
                     '=' -> line[pos + 1].toString()
+                    '>' -> ">" + when (line[pos + 1]) {
+                        '=' -> line[pos + 1].toString()
+                        else -> ""
+                    }
                     else -> ""
                 }
-                else -> ""
+                else -> null
             }
-            else -> null
         }
 
 
+private fun String.sliceUpTo(startIndex: Int, matcher: (Char) -> Boolean): String? {
+    var i = startIndex + 1
+    while (i < length && !matcher(get(i)))
+        ++i
+    return if (i < length) slice(startIndex..i) else null
+}
+
+private fun String.sliceWhile(startIndex: Int, matcher: (Char) -> Boolean): String? {
+    var i = startIndex + 1
+    while (i < length && matcher(get(i)))
+        ++i
+    return slice(startIndex until i)
+}
+
+private fun String.sliceUpTo(startIndex: Int, char: Char): String? =
+        sliceUpTo(startIndex) { it == char }
+
+private fun String.sliceUpTo(startIndex: Int, regex: Regex): String? =
+        sliceUpTo(startIndex) { regex.matches(it.toString()) }
+
+private fun String.sliceWhile(startIndex: Int, char: Char): String? =
+        sliceWhile(startIndex) { it == char }
+
+private fun String.sliceWhile(startIndex: Int, regex: Regex): String? =
+        sliceWhile(startIndex) { regex.matches(it.toString()) }

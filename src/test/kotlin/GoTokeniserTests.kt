07745636ad8cddecc08911f7ea99dd5d5d36dd69
Timur <minteemer@gmail.com>
2018-09-23 22:36:31 +0300

    Tests for identifiers and strings

--- src/test/kotlin/GoTokeniserTests.kt
+++ src/test/kotlin/GoTokeniserTests.kt
@@ -5,6 +5,34 @@ import org.junit.jupiter.api.Test
 
 class GoTokeniserTests {
 
+    private val identifiers = listOf(
+            "a", "_x9", "ThisVariableIsExported", "αβ", "a٩", "ИдентифаерНаВеликомИМогучемРусскомЯзыке"
+    )
+
+    @Test
+    fun identifiersTest() {
+        identifiers.forEach {
+            val token = GoTokenizer(it).nextToken()
+            Assertions.assertEquals(it, token?.lexeme)
+            Assertions.assertTrue(token is IdentifierToken)
+        }
+    }
+
+    private val strings = listOf(
+            "'a'", "'ä'", "'本'", "'\\t'", "'\\000'", "'\\007'", "'\\377'", "'\\x07'", "'\\xff'", "'\\u12e4'",
+            "'\\U00101234'", "'\\''", "`abc`", "`\\n\n\\n`", "\"\\n\"", "\"\\\"\"", "\"Hello, world!\\n\"", "\"日本語\"",
+            "\"\\u65e5本\\U00008a9e\"", "\"\\xff\\u00FF\"", "\"\\U00110000\""
+    )
+
+    @Test
+    fun stringsTest() {
+        strings.forEach {
+            val token = GoTokenizer(it).nextToken()
+            Assertions.assertEquals(it, token?.lexeme)
+            Assertions.assertTrue(token is LiteralToken)
+        }
+    }
+
     private val numbers = listOf(
             "0", "42", "0600", "0xBadFace", "170141183460469231731687303715884105727", "0.", "72.40", "072.40", "2.71828",
             "1.e+0", "6.67428e-11", "1E6", ".25", ".12345E+5", "0i", "011i", "0.i", "2.71828i", "1.e+0i",
